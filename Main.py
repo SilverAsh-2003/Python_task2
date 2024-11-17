ec3d307c9b0828b0008b16658be70e393175fb32
SilverAsh-2003 <sukhadeveashay@gmail.com>
2024-11-17 16:31:49 +0530

    this is first commit

---
+++ Main.py
@@ -0,0 +1,17 @@
+
+from MathOperation.Multop import multivalue
+from MathOperation.Divop import divide
+
+if __name__=='__main__':
+    val1 = int(input('enter first value: '))
+    val2 = int(input('enter second value: '))
+    op = input('which math op you want to perform ?  ')
+
+    if op.lower() =='multi':
+        A1 = multivalue(val1,val2)
+        print(f'multiplication: {A1}')
+    elif op.lower() =='div':
+        A2 = divide(val1,val2)
+        print(f'division: {A2}')
+    else:
+        print('please choose the operation you want perform multi, div')
